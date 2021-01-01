8a85bf4e2e0903f713b9bf004b2de4223daa1e80
Metehan Senol <metehansenol@gmail.com>
2021-01-01 17:30:53 +0300

    search user method fixed

--- instagram.js
+++ instagram.js
@@ -28,24 +28,25 @@ const instagram = {
     // let loginButton = await instagram.page.$x('//div[contains(text(), "Log In")]');
     await instagram.page.keyboard.press("Enter");
     await instagram.page.waitFor(4000);
-    await instagram.page.click('section>div>button')[1];
+    await instagram.page.click("section>div>button")[1];
     await instagram.page.waitFor(3500);
-    await instagram.page.click('div>button')[1];
+    await instagram.page.click("div>button")[1];
     await instagram.page.waitFor(1000);
-
-},
-search: async(searchName) => {
+  },
+  search: async searchName => {
     const url = "https://www.instagram.com/" + searchName + "/";
-    await instagram.page.goto(url, {waitUntil: 'networkidle2'});
+    await instagram.page.goto(url, { waitUntil: "networkidle2" });
 
-    const followingStr = await instagram.page.$eval('ul', el => el.innerHTML);
+    const followingStr = await instagram.page.$eval("ul", el => el.innerHTML);
 
     const followingDom = new JSDOM(followingStr); // TODO: add jsdom npm package and convert to dom
 
     debugger;
     let posts, followers, following;
     posts = followingDom.window.document.body.firstChild.firstChild.firstChild.innerHTML;
-    followers = parseFloat(followingDom.window.document.body.children[1].firstChild.firstChild.getAttribute("title").replace(/,/g, ''));
+    followers = parseFloat(
+      followingDom.window.document.body.children[1].firstChild.firstChild.getAttribute("title").replace(/,/g, "")
+    );
     following = followingDom.window.document.body.children[2].firstChild.firstChild.innerHTML;
     console.log(posts);
     console.log(followers);
@@ -53,27 +54,21 @@ search: async(searchName) => {
 
     //await instagram.page.$x('div > input[placeholder="Search"]',searchName, {delay:50});
     //await instagram.page.type('div > input',searchName, {delay:50});
-},
-searchUser: async(searchString) => {
-  const url = "https://www.instagram.com/web/search/topsearch/?query=" + searchString;
-  instagram.page.on('response', async response => {
-    try{
-      const req = response.request();
-      const orig = await req.url();
-      const text = await response.text();
-      const js = await JSON.parse(text);
-     
-       console.log(js.users[user]);
+  },
+  searchUser: async searchString => {
+    const url = "https://www.instagram.com/web/search/topsearch/?query=" + searchString;
+    const response = await instagram.page.goto(url);
+    console.log(response.status);
+    const responseText = await response.text();
 
-    }catch (err){
-      console.error('Failer getting data from: ${url}');
-      console.error(err);
-    }
-  });
-await instagram.page.goto(url);
-},
-}
+    const data = await JSON.parse(responseText);
 
+    const results = data.users.map(function (item) {
+      return item.user.username;
+    });
 
+    console.log(results);
+  }
+};
 
 module.exports = instagram;
